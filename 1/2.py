7c0d8db0cc8c44d69f6ce98668ed846bca748bee
Daniel Ong <danielong1@gmail.com>
2016-01-27 02:04:37 -0800

    first commit

---
+++ 1/2.py
@@ -0,0 +1,11 @@
+# 1.2 Solution by Daniel Ong
+# Contact: danielong1@gmail.com
+
+def is_permutation(a, b):
+    """Returns true if strings a and b are permutations of each other and false otherwise."""
+    return sorted(a) == sorted(b)
+    
+if __name__ == '__main__':
+    print('foo', 'bar', is_permutation('foo', 'bar'))
+    print('dog', 'god ', is_permutation('dog', 'god '))
+    print('dog', 'god', is_permutation('dog', 'god'))
