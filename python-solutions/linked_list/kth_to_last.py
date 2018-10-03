496612f92a2a4c4e51b79f9ac343b7ff521d3fbb
Daniel Ong <danielong1@gmail.com>
2018-10-02 22:30:22 -0700

    Move test code

---
+++ python-solutions/linked_list/kth_to_last.py
@@ -0,0 +1,29 @@
+
+from typing import Optional
+
+from .linked_list import Node
+
+
+def kth_to_last(k: int, head: Node) -> Optional[Node]:
+    """Returns the kth to last element of a singly linked list
+
+    Args:
+        k: Number of spaces from the end of the list
+        head: Start of linked list
+
+    Returns:
+        The k-th node from the end of the list if valid
+    """
+    ahead = behind = head
+
+    for _ in range(k):  # move ahead k spots forward
+        if ahead is None:
+            return None
+        else:
+            ahead = ahead.next
+
+    while ahead is not None:  # move ahead and behind forward together
+        ahead = ahead.next
+        behind = behind.next
+    return behind
+
--- python-solutions/linked_list/test/test_kth_to_last.py
+++ python-solutions/linked_list/test/test_kth_to_last.py
@@ -1,32 +1,8 @@
 import random
 import unittest
-from typing import Optional
 
-from .linked_list import Node
-
-
-def kth_to_last(k: int, head: Node) -> Optional[Node]:
-    """Returns the kth to last element of a singly linked list
-
-    Args:
-        k: Number of spaces from the end of the list
-        head: Start of linked list
-
-    Returns:
-        The k-th node from the end of the list if valid
-    """
-    ahead = behind = head
-
-    for _ in range(k):  # move ahead k spots forward
-        if ahead is None:
-            return None
-        else:
-            ahead = ahead.next
-
-    while ahead is not None:  # move ahead and behind forward together
-        ahead = ahead.next
-        behind = behind.next
-    return behind
+from ..kth_to_last import kth_to_last
+from ..linked_list import Node
 
 
 class Test(unittest.TestCase):
