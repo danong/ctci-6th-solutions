00d4d297d056f70b0cfa4327ea7fed1416b97fd9
Daniel Ong <danielong1@gmail.com>
2018-10-02 20:50:18 -0700

    Reorganized repo

--- python/1-arrays-and-strings/2.py
+++ python/1-arrays-and-strings/2.py
@@ -4,7 +4,8 @@
 def is_permutation(a, b):
     """Returns true if strings a and b are permutations of each other and false otherwise."""
     return sorted(a) == sorted(b)
-    
+
+
 if __name__ == '__main__':
     print('foo', 'bar', is_permutation('foo', 'bar'))
     print('dog', 'god ', is_permutation('dog', 'god '))
